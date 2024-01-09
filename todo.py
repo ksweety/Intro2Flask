d68f4d7b3bead457362003054b6b0f7b50ad94a6
ksweety <kahlilw03@gmail.com>
2024-01-09 10:28:37 -0500

    wwww

--- todo.py
+++ todo.py
@@ -4,37 +4,39 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
-
-con = pymysql.connect(host='10.100.33.60',
+conn = pymysql.connect(host='10.100.33.60',
                       user='kwilliams4',
                       password='228426581',
-                      database='world',
+                      database='kwilliams4_todo',
                       charset='utf8mb4',
                       cursorclass=pymysql.cursors.DictCursor)
 
+def get_todos():
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT `description` FROM `Todos`")
+        results = cursor.fetchall()
+        todos = [item['description'] for item in results]
+    return todos
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  
-    with con.cursor() as cursor:
-        cursor.execute("SELECT `description` FROM `todos`")
-        results = cursor.fetchall()
-        todos = results
+    todos = get_todos()
 
     if request.method == 'POST':
         new_todo = request.form['new_todo']
-    
-        with con.cursor() as cursor:
-            cursor.execute("INSERT INTO `todos` (`description`) VALUES (%s)", (new_todo,))
-            con.commit()
+        with conn.cursor() as cursor:
+            cursor.execute(f"INSERT INTO `Todos` (`description`) VALUES ('{new_todo}')")
+        conn.commit()
+        todos = get_todos()
 
     return render_template('todo.html.jinja', todos=todos)
 
 @app.route('/delete_todos/<int:todo_index>', methods=['POST'])
 def todo_delete(todo_index):
-   
-    with con.cursor() as cursor:
-        cursor.execute("DELETE FROM `todos` WHERE `description` = %s", (todos[todo_index]['description'],))
-        con.commit()
+    todos = get_todos()
+    with conn.cursor() as cursor:
+        cursor.execute("DELETE FROM `Todos` WHERE `description` = %s", (todos[todo_index],))
+    conn.commit()
 
     return redirect('/')
 
