1c9ce9051e1ed1697782831b1363cb1ba5afa482
ksweety <kahlilw03@gmail.com>
2024-01-11 10:11:36 -0500

    owididda

--- todo.py
+++ todo.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template, redirect
 import pymysql
 import pymysql.cursors
-
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
-
 conn = pymysql.connect(host='10.100.33.60',
                       user='kwilliams4',
                       password='228426581',
@@ -11,9 +11,23 @@ conn = pymysql.connect(host='10.100.33.60',
                       charset='utf8mb4',
                       cursorclass=pymysql.cursors.DictCursor)
 
-# ...
+
+auth = HTTPBasicAuth() 
+
+users = {
+    'ksweety': generate_password_hash("kahlil21")
+} 
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+
 
 @app.route('/', methods=['GET', 'POST'])
+@auth.login_required
 def index():
 
     if request.method == 'POST':
@@ -32,6 +46,7 @@ def index():
     return render_template('todo.html.jinja', todos=results)
 
 @app.route('/delete_todos/<int:todo_index>', methods=['POST'])
+@auth.login_required
 def todo_delete(todo_index):
     with conn.cursor() as cursor:
         cursor.execute(f"DELETE FROM `Todos` WHERE `id` = {todo_index}")
@@ -39,7 +54,9 @@ def todo_delete(todo_index):
 
     return redirect('/') 
 
-@app.route('/complete_todos/<int:todo_index>', methods=['POST'])
+@app.route('/complete_todos/<int:todo_index>', methods=['POST']) 
+@auth.login_required
+
 def complete_todo(todo_index):
     with conn.cursor() as cursor:
         cursor.execute(f"UPDATE `Todos` SET `completions` = 1 WHERE `id` = {todo_index}")
